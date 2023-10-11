85a69d9cb2455023cea6477bc17df9c1baccc705
anandyn2002 <45676373+anandyn2002@users.noreply.github.com>
2023-10-11 06:29:03 +0530

    updates

--- Sources/PMP_Component/IOComponent.swift
+++ Sources/PMP_Component/IOComponent.swift
@@ -3,7 +3,7 @@ import UIKit
 
 open class IOComponent {
     
-    class func createLabel(text: String, font: UIFont, color: UIColor) -> UILabel {
+   open class func createLabel(text: String, font: UIFont, color: UIColor) -> UILabel {
         let lbl = UILabel()
         lbl.font = font
         lbl.textColor = color
@@ -13,7 +13,7 @@ open class IOComponent {
         return lbl
     }
     
-    class func createImageView(radius: CGFloat, bgColor: UIColor) -> UIImageView {
+    open class func createImageView(radius: CGFloat, bgColor: UIColor) -> UIImageView {
         let imgView = UIImageView()
         imgView.contentMode = .scaleAspectFit
         imgView.layer.cornerRadius = radius
@@ -23,7 +23,7 @@ open class IOComponent {
         return imgView
     }
     
-    class func createView(radius: CGFloat = 0, bgColor: UIColor) -> UIView {
+    open class func createView(radius: CGFloat = 0, bgColor: UIColor) -> UIView {
         let view = UIView()
         view.backgroundColor = bgColor
         view.layer.cornerRadius = radius
@@ -31,7 +31,7 @@ open class IOComponent {
         return view
     }
     
-    class func createButton(text: String, radius: CGFloat = 0, titleColor: UIColor, bgColor: UIColor) -> UIButton {
+    open class func createButton(text: String, radius: CGFloat = 0, titleColor: UIColor, bgColor: UIColor) -> UIButton {
         let btn = UIButton()
         btn.setTitle(text, for: .normal)
         btn.setTitleColor( titleColor, for: .normal)
@@ -41,7 +41,7 @@ open class IOComponent {
         return btn
     }
     
-    class func createButtonWith(imageName: String = "", text: String = "",titleColor: UIColor = .clear, radius: CGFloat = 0, bgColor: UIColor) -> UIButton {
+    open class func createButtonWith(imageName: String = "", text: String = "",titleColor: UIColor = .clear, radius: CGFloat = 0, bgColor: UIColor) -> UIButton {
         let btn = UIButton()
         if(!imageName.isEmpty) {
             btn.setImage(UIImage(named: imageName), for: .normal)
@@ -57,7 +57,7 @@ open class IOComponent {
         return btn
     }
     
-    class func createStackView(axisType: NSLayoutConstraint.Axis, list: [UIView]) -> UIStackView {
+    open class func createStackView(axisType: NSLayoutConstraint.Axis, list: [UIView]) -> UIStackView {
         let stackView = UIStackView(arrangedSubviews: list)
         stackView.axis = axisType
         stackView.distribution = .equalSpacing
@@ -67,14 +67,14 @@ open class IOComponent {
         return stackView
     }
     
-    class func createTableView(bgColor: UIColor, style: UITableView.Style = .plain) -> UITableView {
+    open class func createTableView(bgColor: UIColor, style: UITableView.Style = .plain) -> UITableView {
         let tableView = UITableView(frame: .zero, style: style)
         tableView.backgroundColor = bgColor
         tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
     }
     
-    class func createTextField(text: String, textColor: UIColor, font: UIFont) -> DesignableTextField {
+    open class func createTextField(text: String, textColor: UIColor, font: UIFont) -> DesignableTextField {
         let textField = DesignableTextField()
         textField.placeholder = text
         textField.textColor = textColor
@@ -83,7 +83,7 @@ open class IOComponent {
         return textField
     }
     
-    class func createTextView(text: String, textColor: UIColor, font: UIFont) -> UITextView {
+    open class func createTextView(text: String, textColor: UIColor, font: UIFont) -> UITextView {
         let textView = UITextView()
         textView.text = text
         textView.textColor = textColor
@@ -92,7 +92,7 @@ open class IOComponent {
         return textView
     }
     
-    class func createSwitch(tintColor: UIColor, thumbColor: UIColor) -> UISwitch {
+    open class func createSwitch(tintColor: UIColor, thumbColor: UIColor) -> UISwitch {
         let uiSwitch = UISwitch()
         uiSwitch.tintColor = tintColor
         uiSwitch.thumbTintColor = thumbColor
@@ -100,7 +100,7 @@ open class IOComponent {
         return uiSwitch
     }
     
-    class func createSlider(minColor: UIColor, maxColor: UIColor, thumbColor: UIColor) -> UISlider {
+    open class func createSlider(minColor: UIColor, maxColor: UIColor, thumbColor: UIColor) -> UISlider {
         let slider = UISlider()
         slider.minimumTrackTintColor = minColor
         slider.maximumTrackTintColor = maxColor
@@ -109,14 +109,14 @@ open class IOComponent {
         return slider
     }
     
-    class func createScrollview(bgColor: UIColor) -> UIScrollView {
+    open class func createScrollview(bgColor: UIColor) -> UIScrollView {
         let scrollView = UIScrollView()
         scrollView.backgroundColor = bgColor
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         return scrollView
     }
     
-    class func createProgressBar(trackColor: UIColor, progressColor: UIColor) -> UIProgressView {
+    open class func createProgressBar(trackColor: UIColor, progressColor: UIColor) -> UIProgressView {
         let progress = UIProgressView()
         progress.trackTintColor = trackColor
         progress.progressTintColor = progressColor
@@ -124,7 +124,7 @@ open class IOComponent {
         return progress
     }
     
-    class func createSegmentControl(items: [String], font: UIFont, textColor: UIColor) -> UISegmentedControl {
+    open class func createSegmentControl(items: [String], font: UIFont, textColor: UIColor) -> UISegmentedControl {
         let segment = UISegmentedControl(items: items)
         segment.selectedSegmentIndex = 0
         
@@ -134,7 +134,7 @@ open class IOComponent {
         return segment
     }
     
-    class func createCollectionView() -> UICollectionView {
+    open class func createCollectionView() -> UICollectionView {
         let collection = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout.init())
         collection.translatesAutoresizingMaskIntoConstraints = false
         collection.showsVerticalScrollIndicator = false
@@ -142,7 +142,7 @@ open class IOComponent {
         return collection
     }
 
-    class func createRadioButton(size: CGFloat = 30, color: UIColor = .black) -> IORadioButton {
+    open class func createRadioButton(size: CGFloat = 30, color: UIColor = .black) -> IORadioButton {
         let raidoButton = IORadioButton()
         raidoButton.size = CGSize(width: size, height: size)
         raidoButton.btnColor = color
